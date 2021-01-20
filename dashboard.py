3c8eb51e8174f0974ffd996e27f40a64e4ba4d0b
Ayse Yildizhan <53956251+aysey@users.noreply.github.com>
2021-01-20 02:05:32 +0000

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from datetime import datetime
+from datetime import timedelta
+import requests
+import time
+import random
+
+##class for data_generation
+
+
+def data_generation():
+    surr_id = random.randint(1, 3)
+    speed = random.randint(20,200)
+    date = datetime.today().strftime("%Y-%m-%d")
+    time = datetime.now().isoformat()
+
+    return [surr_id, speed, date, time]
+
+
+if __name__ == '__main__':
+
+    REST_API_URL = 'https://api.powerbi.com/beta/e81af6ba-a66f-4cab-90f9-9225862c5cf8/datasets/51a56115-ac32-437a-8f2c-3ed1fa1dc37a/rows?key=24THP%2FqLUg2EWnDtFiTUr8GTjjPOU%2FxjT%2BnkTt9%2FHMlkMG%2B5BhWe0pYVfsJcE8gVNitZ3C2Fp1akv3LR7hLVNQ%3D%3D'
+
+    while True:
+        data_raw = []
+        for i in range(1):
+            row = data_generation()
+            data_raw.append(row)
+            print("Raw data - ", data_raw)
+
+        # set the header record
+        HEADER = ["surr_id", "speed", "date", "time"]
+
+        data_df = pd.DataFrame(data_raw, columns=HEADER)
+        data_json = bytes(data_df.to_json(orient='records'), encoding='utf-8')
+        print("JSON dataset", data_json)
+
+        # Post the data on the Power BI API
+        req = requests.post(REST_API_URL, data_json)
+
+        print("Data posted in Power BI API")
+        time.sleep(2)
